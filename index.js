4a46df4aa080a22bbd73a9d4a32b138775ba591d
Julian Peters <julianpeters@tiot.de>
2018-09-27 18:49:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name){
   return `Happy holidays to ${name}!`
 }
 function happyHolidayTo(holiday, name){
-  return `Happy ${holiday}, {name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
